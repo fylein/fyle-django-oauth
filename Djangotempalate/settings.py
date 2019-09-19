473cf2826b646b9c5bd63bc6a1d6a1150c4a7891
Subhajit <subha@Subhajits-MacBook-Air.local>
2019-09-19 12:03:27 +0530

    Updated code

--- Djangotempalate/settings.py
+++ Djangotempalate/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Djangotempalate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/subha/DjangoProj/Djangotempalate/fyleOauthDjango/templates'],
+        'DIRS': [os.path.join(BASE_DIR ,'fyleOauthDjango/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
